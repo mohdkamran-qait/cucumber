66c816db83efde91dfaf86fd4727a35d06846833
mohdkamran-qait <mohdkamran@qainfotech.com>
2017-08-10 18:06:30 +0530

    after operations

--- src/test/java/cucumberJava/cucumberJava.feature
+++ src/test/java/cucumberJava/cucumberJava.feature
@@ -1,8 +1,26 @@
 Feature: Login
+
+Background:
+	Given user initiate the driver
+
+@First
 Scenario Outline: Login functionality exists
 	Given User navigates to qawebmail
 	When user logs in using Username <username> and Password <password>
-	Then Login is successful	
+	Then Login is successful
+
+
+	
+@First		
 Examples:
 |username|password|                        
 |mohdkamran|Farhan@1710|
+
+
+
+@Second
+Scenario: Logout
+	Given user clicks compose button
+	When user will edit message
+	When user will click send button
+	Then message is send
--- src/test/java/cucumberJava/cucumberJava.java
+++ src/test/java/cucumberJava/cucumberJava.java
@@ -12,35 +12,62 @@ public class cucumberJava {
 
 	WebDriver driver = null;
 
-	
-	
-	@Given("^User navigates to qawebmail$")
-	public void gotogmail() throws InterruptedException {
+	@Given("^user initiate the driver$")
+	public void initdriver() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/Selenium_Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
+	}
+
+	@Given("^User navigates to qawebmail$")
+	public void gotogmail() throws InterruptedException {
+		
 		driver.get("https://webmail.qainfotech.com/");
 		Thread.sleep(1000);
-		
+
 	}
 
 	@When("^user logs in using Username (\\w+@?) and Password (\\w+@?\\d+)$")
-	public void I_enter_Username_as_and_Password_as(String arg1, String arg2) {
+	public void I_enter_Username_as_and_Password_as(String arg1, String arg2) throws InterruptedException {
 		System.out.println(arg1);
 		System.out.println(arg2);
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(arg1);
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(arg2);
 		driver.findElement(By.xpath("//input[@class='ZLoginButton DwtButton']")).click();
+		Thread.sleep(2000);
 	}
 
-	@Then("^Login is successful$") 
+	@Then("^Login is successful$")
 	public void validatelogin() {
 		System.out.println("Successful");
-//		driver.close();
+		// driver.close();
 
-	} 
-		
-	
+	}
 
+	@Given("^user clicks compose button$")
+	public void click_compose_button() throws InterruptedException {
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//div[@id='zb__NEW_MENU']//td[@id='zb__NEW_MENU_title'][text()='New Message']"))
+				.click();
+	}
 
-			
+	@When("^user will edit message$")
+	public void edit_message() {
+		driver.findElement(By.xpath("//input[@id='zv__COMPOSE-1_to_control']")).sendKeys("mohdkamranqait@gmail.com");
+		driver.findElement(By.xpath("//input[@id = 'zv__COMPOSE-1_subject_control']")).sendKeys("CucumberTest");
+		driver.switchTo().frame("ZmHtmlEditor1_body_ifr");
+		driver.findElement(By.xpath("")).sendKeys("Helllloooo!!!!!");
+
+	}
+
+	@When("^user will click send button$")
+	public void send_message() {
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath("//body[@aria-label ='Compose body']")).click();
+	}
+
+	@Then("^message is send$")
+	public void msg_success() {
+		System.out.println("Message is sent successfully!!!!!!!!!!!!!!!!!!!");
+	}
+
 }
